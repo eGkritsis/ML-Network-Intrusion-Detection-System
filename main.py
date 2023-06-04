73ed0abf3f93f68037ed15b356afb6321bfda5d4
eGkritsis <tzenios13@gmail.com>
2023-06-04 17:38:43 +0300

    updates

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from capture.capture_packets import capture_packets
 from capture.capture_packets import captured_packets
 from anomaly_detection.detect_anomalies import detect_anomaly
+from visualization.visualize import plot_anomaly_score, time_series_anomaly_plot
 
 
 # Entry point of the packet capture
@@ -16,6 +17,8 @@ if __name__ == '__main__':
     capture_packets(interface, num_packets)
  
     # Detect anomalies and generate report
-    detect_anomaly(captured_packets)
+    anomaly_scores = detect_anomaly(captured_packets)
 
+    # Produce a plot about anomaly scores
+    plot_anomaly_score(anomaly_scores)
 
