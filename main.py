b6ff2700d68735ffd6423e56eadc80ac8bd66cd1
eGkritsis <tzenios13@gmail.com>
2023-06-06 21:30:43 +0300

    Updated main

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from capture.capture_packets import capture_packets
-from capture.capture_packets import captured_packets
-from anomaly_detection.detect_anomalies import detect_anomaly
 from visualization.visualize import plot_anomaly_score, time_series_anomaly_plot
+from anomaly_detection import detect_malicious_traffic
+from anomaly_detection.detect_malicious_traffic import load_model, detection
 
 
 # Entry point of the packet capture
@@ -11,14 +11,25 @@ if __name__ == '__main__':
     interface = 'Wi-Fi'  
 
     # Set the number of packets to capture (0 for unlimited)
-    num_packets = 100
+    num_packets = 10
 
     # Start capturing packets
-    capture_packets(interface, num_packets)
- 
+    captured_packets = capture_packets(interface, num_packets)
+
     # Detect anomalies and generate report
-    anomaly_scores = detect_anomaly(captured_packets)
+    #anomaly_scores = detect_anomaly(captured_packets)
 
     # Produce a plot about anomaly scores
-    plot_anomaly_score(anomaly_scores)
+    #plot_anomaly_score(anomaly_scores)
 
+    # Model file
+    model_file = r'D:\AUEB\Projects\Network-Traffic-Analyzer\model.pkl'
+
+    # Load model
+    model = load_model(model_file)
+
+    # Detect anomalies and generate report
+    predicted_labels = detection(captured_packets, model)
+    generate_report(predicted_labels)
+
+    
