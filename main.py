bd62615f8f47bae62cfc4b71d32dc6a153f5c7d9
eGkritsis <tzenios13@gmail.com>
2023-06-09 22:24:06 +0300

    Update main

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-from capture.capture_packets import capture_packets
+from capture.capture_traffic import capture_packets, capture_flows
 from visualization.visualize import plot_anomaly_score, time_series_anomaly_plot
-from anomaly_detection import detect_malicious_traffic
-from anomaly_detection.detect_malicious_traffic import load_model, detection
+from anomaly_detection import supervised_learning, unsupervised_learning
+from anomaly_detection.supervised_learning import load_model, flow_detection, packet_detection, generate_report
+
+# Tha doume einai gia to unsupervised isws fugei
+from anomaly_detection.unsupervised_learning import detect_anomaly    
+
 
 
 # Entry point of the packet capture
@@ -11,11 +15,14 @@ if __name__ == '__main__':
     interface = 'Wi-Fi'  
 
     # Set the number of packets to capture (0 for unlimited)
-    num_packets = 10
+    num_packets = 100
 
     # Start capturing packets
-    captured_packets = capture_packets(interface, num_packets)
+    #captured_packets = capture_packets(interface, num_packets)
 
+    # Start capturing flows
+    captured_flows = capture_flows(interface, num_packets)
+    
     # Detect anomalies and generate report
     #anomaly_scores = detect_anomaly(captured_packets)
 
@@ -23,13 +30,13 @@ if __name__ == '__main__':
     #plot_anomaly_score(anomaly_scores)
 
     # Model file
-    model_file = r'D:\AUEB\Projects\Network-Traffic-Analyzer\model.pkl'
+    model_file = r'\Network-Traffic-Analyzer\model.pkl'
 
     # Load model
     model = load_model(model_file)
 
     # Detect anomalies and generate report
-    predicted_labels = detection(captured_packets, model)
+    predicted_labels = flow_detection(captured_flows, model)
     generate_report(predicted_labels)
 
     
