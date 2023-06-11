37433d2b0b50516e9e45933ceaf06c78de82f011
eGkritsis <tzenios13@gmail.com>
2023-06-12 01:45:05 +0300

    cleanup

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from capture.capture_traffic import capture_flows
-from anomaly_detection.supervised_learning import load_model, flow_detection, generate_report_label, generate_report_label_attack_cat  
+from anomaly_detection.supervised_learning import load_model, flow_detection, generate_report_label, generate_report_label_attack_cat,generate_report_attack_cat  
 
 def main():
     # Set the network interface for packet capturing 
     # 'Ethernet' or 'Wi-Fi'
-    interface = 'Wi-Fi'  
+    interface = 'Ethernet'  
 
     # Set the number of packets to capture (0 for unlimited)
-    num_packets = 100
+    num_packets = 1000
     # Start capturing packets
     #captured_packets = capture_packets(interface, num_packets)
 
@@ -20,11 +20,23 @@ def main():
     # Start capturing flows
     captured_flows = capture_flows(interface, num_packets)
 
-    # Model file (1 target)
-    model_file = r'model_label.pkl'
+    # Model file (Target = label)
+    #model_file = r'model_label.pkl'
 
-    # Model file (2 targets)
+    # Model file (Target = attack_cat)
+    #model_file = r'model_cat.pkl'
+
+    # Model file (Targets = label + attack_cat)
     #model_file = r'model_label_attack_cat.pkl'
+
+    # Model file (Targets = label - NAIVE BAYES)
+    #model_file = r'naive_label.pkl'
+
+    # Model file (Targets = label - MLP)
+    #model_file = r'mlp_label.pkl'
+
+    # Model fie (Targets = label - GBC)
+    model_file = r'gbc_training.pkl'
 
     # Load model
     model = load_model(model_file)
@@ -32,10 +44,13 @@ def main():
     # Detect malicious traffic
     predicted_labels = flow_detection(captured_flows, model)
 
-    # Generate report (1 target)
+    # Generate report (Target = label)
     generate_report_label(predicted_labels)
 
-    # Generate report (2 targets)
+    # Generate report (Target = attack_cat)
+    #generate_report_attack_cat(predicted_labels)
+
+    # Generate report (Target = label + attack_cat)
     #generate_report_label_attack_cat(predicted_labels)
 
 
