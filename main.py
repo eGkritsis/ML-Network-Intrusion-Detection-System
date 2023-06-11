c485ac1f628e3edce901a74e72fa4c79b4a25735
eGkritsis <tzenios13@gmail.com>
2023-06-11 20:24:20 +0300

    Updated main.py

--- main.py
+++ main.py
@@ -1,22 +1,13 @@
-from capture.capture_traffic import capture_packets, capture_flows
-from visualization.visualize import plot_anomaly_score, time_series_anomaly_plot
-from anomaly_detection import supervised_learning, unsupervised_learning
-from anomaly_detection.supervised_learning import load_model, flow_detection, packet_detection, generate_report
+from capture.capture_traffic import capture_flows
+from anomaly_detection.supervised_learning import load_model, flow_detection, generate_report_label, generate_report_label_attack_cat  
 
-# Tha doume einai gia to unsupervised isws fugei
-from anomaly_detection.unsupervised_learning import detect_anomaly    
-
-
-
-# Entry point of the packet capture
-if __name__ == '__main__':
+def main():
     # Set the network interface for packet capturing 
     # 'Ethernet' or 'Wi-Fi'
     interface = 'Wi-Fi'  
 
     # Set the number of packets to capture (0 for unlimited)
     num_packets = 100
-
     # Start capturing packets
     #captured_packets = capture_packets(interface, num_packets)
 
@@ -29,14 +20,28 @@ if __name__ == '__main__':
     # Start capturing flows
     captured_flows = capture_flows(interface, num_packets)
 
-    # Model file
-    model_file = r'\Network-Traffic-Analyzer\model.pkl'
+    # Model file (1 target)
+    model_file = r'model_label.pkl'
+
+    # Model file (2 targets)
+    #model_file = r'model_label_attack_cat.pkl'
 
     # Load model
     model = load_model(model_file)
 
-    # Detect anomalies and generate report
+    # Detect malicious traffic
     predicted_labels = flow_detection(captured_flows, model)
-    generate_report(predicted_labels)
+
+    # Generate report (1 target)
+    generate_report_label(predicted_labels)
+
+    # Generate report (2 targets)
+    #generate_report_label_attack_cat(predicted_labels)
+
+
+# Entry point of the packet capture
+if __name__ == '__main__':
+    main()
+    
 
     
