454cbf9bb4f32bf72cb862ae81112898ff22e0f9
eGkritsis <tzenios13@gmail.com>
2023-07-03 19:10:49 +0300

    Updated main

--- main.py
+++ main.py
@@ -1,26 +1,20 @@
 from capture.capture_traffic import capture_flows
-from traffic_analysis.IDS import load_model, supervised_flow_detection, generate_report_label, generate_report_label_attack_cat, generate_report_attack_cat, generate_anomaly_report, unsupervised_flow_detection
+from traffic_analysis.IDS import load_model, supervised_flow_detection, generate_report_attack_cat, unsupervised_flow_detection
 
 
 def main():
     # Set the network interface for packet capturing 
-    # 'Ethernet' or 'Wi-Fi'
+    # 'Ethernet' or 'Wi-Fi' or 'Adapter for loopback traffic capture'
     interface = 'Wi-fi'  
 
-    # Set the number of packets to capture (0 for unlimited)
-    num_packets = 1000
+    # Set the target IP or the range of IP addresses you want to capture traffic from
+    target_ip = get_local_ip()
 
-    # Start capturing packets
-    #captured_packets = capture_packets(interface, num_packets)
-
-    # Detect anomalies and generate report
-    #anomaly_scores = detect_anomaly(captured_packets)
-
-    # Produce a plot about anomaly scores
-    #plot_anomaly_score(anomaly_scores)
+    # Set the display filter
+    filter = f'ip.addr=={target_ip}'
 
     # Start capturing flows
-    captured_flows = capture_flows(interface, num_packets)
+    captured_flows = capture_flows(interface, filter)
 
     # Model file 
     model_file = r'rf_attack_cat_mapped.pkl'
