f0ecf4ca51e9f9d75881df37d546939422948100
eGkritsis <tzenios13@gmail.com>
2023-07-03 19:08:23 +0300

    cleanup

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 from capture.capture_traffic import capture_flows
 from traffic_analysis.IDS import load_model, supervised_flow_detection, generate_report_label
+from traffic_analysis.IDS import get_local_ip
 
 def main():
     # Set the network interface for packet capturing 
-    # 'Ethernet' or 'Wi-Fi'
+    # 'Ethernet' or 'Wi-Fi' or 'Adapter for loopback traffic capture'
     interface = 'Wi-fi'  
 
+    # Set the target IP or the range of IP addresses you want to capture traffic from
+    target_ip = get_local_ip()
+
+    # Set the display filter
+    filter = f'ip.addr=={target_ip}'
+
     # Start capturing flows
-    captured_flows = capture_flows(interface)
+    captured_flows = capture_flows(interface, filter)
 
     # Model file 
-    model_file = r'rf_31_kdd.pkl'
+    model_file = r'rf_kdd.pkl'
 
     # Load model
     model = load_model(model_file)
@@ -21,15 +28,8 @@ def main():
     # Generate report (Target = label)
     generate_report_label(predicted_labels)
 
-    # UNSUPERVISED LEARNING
-    #unsupervised_flow_detection(captured_flows)
-
-    #plot_attack_categories(predicted_labels)
-
-
 # Entry point of the packet capture
 if __name__ == '__main__':
     main()
     
 
-    
